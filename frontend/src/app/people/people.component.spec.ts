134e0e53454502a08b76fa9db97def8d5300f261
Tsvetan Tsvetkov <tsvetan.tsvetkov@dedalus.eu>
2023-01-12 09:09:42 +0100

    fixed unit tests

--- frontend/src/app/people/people.component.spec.ts
+++ frontend/src/app/people/people.component.spec.ts
@@ -1,19 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { PeopleComponent } from './people.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Config, ConfigService } from '../config.service';
+import { PeopleComponent } from './people.component';
 
 describe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      imports: [HttpClientTestingModule],
-      declarations: [ PeopleComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, ReactiveFormsModule, FormsModule],
+      declarations: [PeopleComponent],
+    }).compileComponents();
+
+    configService = TestBed.inject(ConfigService);
+    const configDummy: Config = {
+      backendUrl: '/TEST_DUMMY_BACKEND_URL',
+    };
+
+    spyOn(configService, 'getConfig').and.returnValue(configDummy);
 
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
@@ -21,6 +30,7 @@ describe('PeopleComponent', () => {
   });
 
   it('should create', () => {
+    console.log(configService.getConfig());
     expect(component).toBeTruthy();
   });
 });
